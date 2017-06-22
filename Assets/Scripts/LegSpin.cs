e03f8d495770cdfd59511482df69d7669d2bbccf
TheRogueNinja <shreyassawant018@gmail.com>
2017-06-22 12:59:29 +0530

    Unity Game

---
+++ Assets/Scripts/LegSpin.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LegSpin : MonoBehaviour {
+	public float spinFactor;
+	void OnCollisionEnter(Collision col){
+		Debug.Log("You are in Spin Block");
+
+	}
+
+
+}
---
+++ Assets/Scripts/SpinBall.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinBall : MonoBehaviour {
+	public Rigidbody projectile;
+    public Transform shotPos;
+    public float shotForce = 5f;
+    public float moveSpeed = 10f;
+    
+    void Update ()
+    {
+
+        float h = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        float v = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+        
+        transform.Translate(new Vector3(h, v, 0f));
+        
+        if(Input.GetButtonUp("Fire1"))
+        {   
+            
+            Rigidbody shot = Instantiate(projectile, shotPos.position, shotPos.rotation);
+            //shot.AddForce(shotPos.forward* shotForce);
+            shot.transform.position = transform.position+Camera.main.transform.forward * 2;
+            shot.velocity = Camera.main.transform.forward * 50;
+
+            //shot.transform.rotation = Quaternion.AngleAxis(30, Vector3.up);
+        }
+        
+    }
+}
